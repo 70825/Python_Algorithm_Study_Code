ccde33b49382784a6932cdbea904cefa2363239b
Dabeen Jeong <hello70825@gmail.com>
2021-03-16 15:11:35 +0900

    update

---
+++ 2nd_week/다양한 출력 방법/20540.py
@@ -0,0 +1,13 @@
+s = input()
+
+if s[0] == 'E':print('I', end='')
+else: print('E', end='')
+
+if s[1] == 'S':print('N', end='')
+else: print('S', end='')
+
+if s[2] == 'T':print('F', end='')
+else: print('T', end='')
+
+if s[3] == 'J': print('P')
+else: print('J')
